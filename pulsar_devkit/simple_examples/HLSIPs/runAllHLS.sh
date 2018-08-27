043c9a0ff12d9eb8fcc8be072f17ac413a0691d9
Sven Dildick <dildick@cern.ch>
2018-08-27 14:08:02 -0500

    update scripts

--- pulsar_devkit/simple_examples/HLSIPs/runAllHLS.sh
+++ pulsar_devkit/simple_examples/HLSIPs/runAllHLS.sh
@@ -1,9 +1,4 @@
-vivado_hls run_hls_prop.tcl
-vivado_hls run_hls_match.tcl
-vivado_hls run_hls_matchprop.tcl
-vivado_hls run_hls_calc_pt.tcl
-vivado_hls run_hls_calc_eta.tcl
-vivado_hls run_hls_calc_phi.tcl
+./runMainHLS.sh
 
 #LUTs
 vivado_hls run_hls_deta_LUT.tcl 
--- pulsar_devkit/simple_examples/HLSIPs/runMainHLS.sh
+++ pulsar_devkit/simple_examples/HLSIPs/runMainHLS.sh
@@ -4,3 +4,4 @@ vivado_hls run_hls_matchprop.tcl
 vivado_hls run_hls_calc_pt.tcl
 vivado_hls run_hls_calc_eta.tcl
 vivado_hls run_hls_calc_phi.tcl
+vivado_hls run_hls_multimatch.tcl
